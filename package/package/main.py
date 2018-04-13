d0b55a6ad7604307616404f213cf6bc106ce1faa
Emiliano Saenz <32196924+saenzemiliano@users.noreply.github.com>
2018-04-13 13:04:00 -0300

    Packaging
    Install user package
    python3 setup.py install
    
    Remove user package
    pip uninstall [name-package]

---
+++ package/package/main.py
@@ -0,0 +1,31 @@
+import datetime
+from src.model.person import Person
+
+
+jane = Person(
+    "Jane",
+    "Doe",
+    datetime.date(1992, 3, 12), # year, month, day
+    "No. 12 Short Street, Greenville",
+    "555 456 0987",
+    "jane.doe@example.com"
+)
+print(dir(jane))
+
+print(jane.fullname)
+
+jane.fullname = "Jane Doe"
+print(jane.fullname)
+print(jane.name)
+print(jane.surname)
+
+
+jane.add_pet("cat")
+print(jane.pets)
+
+
+print(jane.allowed_titles_starting_with("M"))
+print(Person.allowed_titles_starting_with("M"))
+
+print(jane.allowed_titles_ending_with("s"))
+print(Person.allowed_titles_ending_with("s"))
---
+++ package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='package',
+    version='1.1',
+    description='Our first package',
+    url='http://saenz.com',
+    author='Saenz',
+    author_email='saenz@example.com',
+    license='GPL',
+    packages=['package'],
+    zip_safe=False,
+)
